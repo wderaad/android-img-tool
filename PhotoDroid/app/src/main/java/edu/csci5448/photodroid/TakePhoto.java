d504520fa62f7c361e50ac0c44069d3c2440d6b6
derekreiersen <derek.reiersen@colorado.edu>
2015-11-20 10:52:28 -0700

    Added way to get back to camera once left

--- PhotoDroid/app/src/main/java/edu/csci5448/photodroid/TakePhoto.java
+++ PhotoDroid/app/src/main/java/edu/csci5448/photodroid/TakePhoto.java
@@ -32,6 +32,25 @@ public class
     }
 
     @Override
+    protected void onPause() {
+        super.onPause();
+        mCamera.release();
+        mCamera= null;
+
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        try {
+            mCamera = Camera.open();
+        }
+        catch(Exception e) {
+        }
+
+
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
