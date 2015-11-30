19ddb0a2628c68ed583e21b06d558316cb21f519
BenedictRM <mehringr@gmail.com>
2015-11-29 22:55:13 -0700

    Images now save
    images now save to a PhotoDroid folder under Pictures, but for some
    reason my Gallery doesn't recognize them as jpg until I rename them
    manually

--- PhotoDroid/app/src/main/java/edu/csci5448/photodroid/TakePhoto.java
+++ PhotoDroid/app/src/main/java/edu/csci5448/photodroid/TakePhoto.java
@@ -1,25 +1,38 @@
 package edu.csci5448.photodroid;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.hardware.Camera;
-import android.view.animation.AlphaAnimation;
+import android.widget.Button;
 import android.widget.FrameLayout;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 // Some code adapted from http://developer.android.com/guide/topics/media/camera.html#custom-camera
-public class
-        TakePhoto extends AppCompatActivity {
-        private Camera mCamera;
-        private CameraPreview mPreview;
-        private static final String TAG = MainActivity.class.getSimpleName();
+@SuppressWarnings("deprecation")
+public class TakePhoto extends AppCompatActivity {
+
+    private Camera mCamera;
+    private CameraPreview mPreview;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private Button imageButton;//for taking photos
+    //For saving images
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    public static final int MEDIA_TYPE_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +47,15 @@ public class
         mPreview= new CameraPreview(this,mCamera);
         FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
         preview.addView(mPreview);
+
+        //Initialize button listener for taking photo
+        addListenerOnButton();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        //Release camera resources
         mCamera.release();
         mCamera= null;
 
@@ -75,5 +92,79 @@ public class
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    //To retrieve photo data http://www.mkyong.com/android/android-imagebutton-selector-example/
+    public void addListenerOnButton() {
+
+        imageButton = (Button) findViewById(R.id.button_capture);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Log.w(TAG, "Taking Photo!\n");
+                // get an image from the camera
+                mCamera.takePicture(null, null, mPicture);
+            }
+        });
+    }
+
+    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+            //Returns generic name for image
+            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+            if (pictureFile == null){
+                Log.d(TAG, "Error creating media file, check storage permissions: ");
+                return;
+            }
+
+            try {
+                Log.w(TAG, "Saving!\n");
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+                Log.w(TAG, "Saved!\n");
+            } catch (FileNotFoundException e) {
+                Log.d(TAG, "File not found: " + e.getMessage());
+            } catch (IOException e) {
+                Log.d(TAG, "Error accessing file: " + e.getMessage());
+            }
+        }
+    };
+
+    /** Create a File for saving an image or video */
+    private static File getOutputMediaFile(int type){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "PhotoDroid");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (!mediaStorageDir.mkdirs()){
+                Log.d("PhotoDroid", "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +"PhotoDroid"+ timeStamp + ".jpg");
+            Log.w(TAG, "Created Media file" + mediaFile + "\n");
+        }
+        else {
+            Log.w(TAG, "Returning null... not good!\n");
+            return null;
+        }
+
+        return mediaFile;
     }
 }
