bc57caee2e5b6820609e74b2945e6587eb8d3b7c
BenedictRM <mehringr@gmail.com>
2015-12-01 17:54:49 -0700

    Sharpen effect added
    I created my own filter to run over the image, works pretty swell

--- PhotoDroid/app/src/main/java/edu/csci5448/photodroid/EditPhoto.java
+++ PhotoDroid/app/src/main/java/edu/csci5448/photodroid/EditPhoto.java
@@ -46,44 +46,97 @@ public class EditPhoto extends AppCompatActivity {
         setPhoto(tempPhoto);
         getPhoto().setImageResource();//check photo was set correctly
         tempPhoto = null;//return to garbage collector
+
+        return;
     }
 
 
     public void onDiscardPhoto(View v){
         this.finish();
+        return;
     }
 
     public void onSavePhoto(View v){
-
+        return;
     }
 
-    public void onBlurSelected(View v){
+    //this method redisplays the edited bitmap
+    public void redisplay(Bitmap resultBitmap)
+    {
         Photo tempPhoto;
-        //Converting bitmap to Mat
-        Mat ImageMat = new Mat (getBitmap().getHeight(), getBitmap().getWidth(), CvType.CV_8U, new Scalar(4));
-        Bitmap myBitmap32 = getBitmap().copy(Bitmap.Config.ARGB_8888, true);
-        Utils.bitmapToMat(myBitmap32, ImageMat);
-        //Set kernel size:
-        org.opencv.core.Size size = new Size(25,25);
 
-        //Do image processing (Blur) with opencv here
-        Imgproc.GaussianBlur(ImageMat, ImageMat,size, 4);
-
-        //Convert Mat back to bitmap
-        Bitmap resultBitmap = Bitmap.createBitmap(ImageMat.cols(),  ImageMat.rows(),Bitmap.Config.ARGB_8888);;
-        Utils.matToBitmap(ImageMat, resultBitmap);
-
-        //Set object bitmap to the result
+        //set new bitmap
         setBitmap(resultBitmap);
 
-        //*****THIS SHOULD BE ITS OWN METHOD EVENTUALLY*****
-        //Stub--probably need to return to the image display here
-        //For now just redisplay inside method
-        Log.d(TAG, "Redisplaying blurred image\n");
+        Log.d(TAG, "Redisplaying edited image\n");
         tempPhoto = new Photo(getBitmap(),(ImageView)findViewById(R.id.EditPhotoImageView));
         setPhoto(tempPhoto);
         getPhoto().setImageResource();//check photo was set correctly
         tempPhoto = null;//return to garbage collector
+
+        return;
+    }
+
+    //Blur effect
+    public void onBlurSelected(View v){
+        //Set kernel size of blur effect (can be increase/decreased):
+        org.opencv.core.Size size = new Size(25,25);//filter levels
+        int sigmaX = 4;//filter width
+
+        //Converting bitmap to Mat
+        Mat imageMat = new Mat (getBitmap().getHeight(), getBitmap().getWidth(), CvType.CV_8UC3, new Scalar(4));
+        Bitmap myBitmap32 = getBitmap().copy(Bitmap.Config.ARGB_8888, true);
+        Utils.bitmapToMat(myBitmap32, imageMat);
+
+
+        //Do image processing (Blur) with opencv here
+        Imgproc.GaussianBlur(imageMat, imageMat, size, sigmaX);
+
+        //Convert Mat back to bitmap
+        Bitmap resultBitmap = Bitmap.createBitmap(imageMat.cols(),imageMat.rows(),Bitmap.Config.ARGB_8888);;
+        Utils.matToBitmap(imageMat, resultBitmap);
+
+        //Redisplay edited image
+        redisplay(resultBitmap);
+
+        return;
+    }
+
+    //Sharpen filter
+    public void onSharpenSelected(View v){
+        //Set kernel size of blur effect (can be increase/decreased):
+        org.opencv.core.Size size = new Size(0,0);//gaussian smoothing levels
+        int sigmaX = 10;//filter width
+        Mat imageMatCopy;//for subtracting from original image
+
+        //Converting bitmap to Mat
+        Mat imageMat = new Mat (getBitmap().getHeight(), getBitmap().getWidth(), CvType.CV_8UC3);
+        Bitmap myBitmap32 = getBitmap().copy(Bitmap.Config.ARGB_8888, true);
+        Utils.bitmapToMat(myBitmap32, imageMat);
+
+        //create own kernel to run over image
+        Mat kernel = new Mat(3, 3, CvType.CV_32F);//create 3x3 matrix
+        //.put(row,col,value)
+        kernel.put(0,0,-1);
+        kernel.put(0,1,-1);
+        kernel.put(0,2,-1);
+        kernel.put(1,0,-1);
+        kernel.put(1,1,9);
+        kernel.put(1,2,-1);
+        kernel.put(2,0,-1);
+        kernel.put(2,1,-1);
+        kernel.put(2,2,-1);
+
+        Imgproc.filter2D(imageMat, imageMat, -1, kernel);
+
+        //Convert Mat back to bitmap
+        Bitmap resultBitmap = Bitmap.createBitmap(imageMat.cols(),imageMat.rows(),Bitmap.Config.ARGB_8888);;
+        Utils.matToBitmap(imageMat, resultBitmap);
+
+        //Redisplay edited image
+        redisplay(resultBitmap);
+
+        return;
     }
 
     //getter for mainPhoto
