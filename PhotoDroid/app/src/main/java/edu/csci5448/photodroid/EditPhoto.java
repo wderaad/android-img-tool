04de6dfcd3290448128712bec036e4297d1ee8dd
BenedictRM <mehringr@gmail.com>
2015-12-01 19:52:05 -0700

    Contours filter working
    This one was a bit of a pain but looks super cool

--- PhotoDroid/app/src/main/java/edu/csci5448/photodroid/EditPhoto.java
+++ PhotoDroid/app/src/main/java/edu/csci5448/photodroid/EditPhoto.java
@@ -13,11 +13,16 @@ import android.widget.ImageView;
 import org.opencv.android.Utils;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class EditPhoto extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -98,7 +103,7 @@ public class EditPhoto extends AppCompatActivity {
         Imgproc.GaussianBlur(imageMat, imageMat, size, sigmaX);
 
         //Convert Mat back to bitmap
-        Bitmap resultBitmap = Bitmap.createBitmap(imageMat.cols(),imageMat.rows(),Bitmap.Config.ARGB_8888);;
+        Bitmap resultBitmap = Bitmap.createBitmap(imageMat.cols(),imageMat.rows(),Bitmap.Config.ARGB_8888);
         Utils.matToBitmap(imageMat, resultBitmap);
 
         //Redisplay edited image
@@ -119,11 +124,62 @@ public class EditPhoto extends AppCompatActivity {
         Bitmap myBitmap32 = getBitmap().copy(Bitmap.Config.ARGB_8888, true);
         Utils.bitmapToMat(myBitmap32, imageMat);
 
+        //Perform image sharpening with custom filter
         Imgproc.filter2D(imageMat, imageMat, -1, Sharpenkernel);
 
         //Convert Mat back to bitmap
-        Bitmap resultBitmap = Bitmap.createBitmap(imageMat.cols(),imageMat.rows(),Bitmap.Config.ARGB_8888);;
+        Bitmap resultBitmap = Bitmap.createBitmap(imageMat.cols(),imageMat.rows(),Bitmap.Config.ARGB_8888);
         Utils.matToBitmap(imageMat, resultBitmap);
+
+        //Redisplay edited image
+        redisplay(resultBitmap);
+
+        return;
+    }
+
+    //Contours effect: Contours can be explained simply as a curve joining all the continuous points (along the boundary), having same color or intensity.
+    public void onContoursSelected(View v){
+        //Set kernel size of blur effect (can be increase/decreased):
+        org.opencv.core.Size size = new Size(3,3);//filter levels
+        int thresh = 100;
+        Mat imageMatGray;
+        //For filter
+        Mat hierarchy = new Mat();
+        List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+        Random random = new Random();
+        Scalar color = new Scalar(random.nextInt(255), random.nextInt(255),random.nextInt(255));
+
+        //Converting bitmap to Mat
+        Mat imageMat = new Mat (getBitmap().getHeight(), getBitmap().getWidth(), CvType.CV_8UC3, new Scalar(4));
+        Bitmap myBitmap32 = getBitmap().copy(Bitmap.Config.ARGB_8888, true);
+        Utils.bitmapToMat(myBitmap32, imageMat);
+
+        //initialize image gray
+        imageMatGray = new Mat (getBitmap().getHeight(), getBitmap().getWidth(), CvType.CV_8UC3, new Scalar(4));
+        Utils.bitmapToMat(myBitmap32, imageMatGray);
+
+        //Convert image to gray and blur it
+        Imgproc.cvtColor(imageMat, imageMatGray, Imgproc.COLOR_RGB2GRAY);
+        Imgproc.blur(imageMatGray, imageMatGray, size);
+        //Now find edges with Canny algorithm
+        Imgproc.Canny(imageMatGray, imageMat, thresh, thresh*2);
+        //Now find contours
+        Imgproc.findContours(imageMat, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+
+        //Now draw the contours
+        Mat drawing = Mat.zeros(imageMatGray.size(), CvType.CV_8UC3);
+        for(int i = 0; i< contours.size(); i++ ) {
+            //Declare random numer between 0 and 255 to color pixel contour
+            color.val[0] = random.nextInt(255);
+            color.val[1] = random.nextInt(255);
+            color.val[2] = random.nextInt(255);
+            //Imgproc.drawContours(drawing, contours, i, color, 2, 8, hierarchy, 0, new Point() );
+            Imgproc.drawContours(drawing, contours, i, color, 8);
+        }
+
+        //Convert Mat back to bitmap
+        Bitmap resultBitmap = Bitmap.createBitmap(drawing.cols(),drawing.rows(),Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(drawing, resultBitmap);
 
         //Redisplay edited image
         redisplay(resultBitmap);
